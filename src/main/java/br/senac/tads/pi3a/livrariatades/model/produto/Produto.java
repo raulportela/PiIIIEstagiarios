c5d833179b15903cc32b0afab9779d55b368f881
erikapalms <palmeira_souza@yahoo.com.br>
2018-10-24 12:38:39 -0200

    Adicionado model do produto

--- src/main/java/br/senac/tads/pi3a/livrariatades/model/produto/Produto.java
+++ src/main/java/br/senac/tads/pi3a/livrariatades/model/produto/Produto.java
@@ -7,8 +7,90 @@ package br.senac.tads.pi3a.livrariatades.model.produto;
 
 /**
  *
- * @author Raul de Paula
+ * @author erikapalms
  */
 public class Produto {
+    
+    private long id;
+    private String titulo;
+    private String autor;
+    private String editora;
+    private String descricao;
+    private Integer quantidade;
+    private boolean disponivel;
+    private Float valor;
+    
+    public Produto (String titulo, String autor, String editora, String descricao,
+            Integer quantidade, boolean disponivel, Float valor){
+        
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.disponivel = disponivel;
+        this.valor = valor;
+        
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
     
 }
